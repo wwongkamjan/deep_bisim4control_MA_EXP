3adb52a0e7e8de1943fdebe0ef83073b4fd9d07b
wwongkamjan <w.wongkamjan@gmail.com>
2022-04-20 17:15:39 -0700

    add eval resource

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=out/cheetah_bisim_out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
 #SBATCH --error=out/cheetah_bisim_err_out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
 #SBATCH --time=48:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
-#SBATCH --qos=default                                       # set QOS, this will determine what resources can be requested
+#SBATCH --qos=medium                                     # set QOS, this will determine what resources can be requested
 #SBATCH --mem=16GB                                              # memory required by job; if unit is not specified MB will be assumed
 #SBATCH --gres=gpu:1
 #SBATCH --account=furongh
@@ -14,6 +14,6 @@ module add Python3/3.6.4
 
 export MJLIB_PATH=/nfshomes/wwongkam/.mujoco/mujoco200/bin/libmujoco200.so
 
-srun bash -c "hostname; python train.py --resource_files 'distractors/images/*.mp4' --domain_name cheetah --task_name run --encoder_type pixel --decoder_type identity --action_repeat 4 --save_video --save_tb --work_dir ./log --seed 1"
+srun bash -c "hostname; python train.py --eval_resource_files 'distractors/images/*.mp4' --resource_files 'distractors/images/*.mp4' --domain_name cheetah --task_name run --encoder_type pixel --decoder_type identity --action_repeat 4 --save_video --save_tb --work_dir ./log --seed 1"
 
 # once the end of the batch script is reached your job allocation will be revoked
