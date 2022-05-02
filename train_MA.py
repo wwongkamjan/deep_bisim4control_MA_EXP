195923503194409481493cc67a42561c9db06868
wwongkamjan <w.wongkamjan@gmail.com>
2022-05-01 23:14:51 -0700

    framestack fix

--- train_MA.py
+++ train_MA.py
@@ -251,10 +251,10 @@ def main():
 
     # stack several consecutive frames together
     if args.encoder_type.startswith('pixel'):
-        env = ss.resize_v0(env, x_size=84, y_size=84)
+        env = ss.resize_v0(env, x_size=100, y_size=100)
         env = ss.frame_stack_v1(env, args.frame_stack)
         # env = utils.FrameStack(env, k=args.frame_stack)
-        eval_env = ss.resize_v0(eval_env, x_size=84, y_size=84)
+        eval_env = ss.resize_v0(eval_env, x_size=100, y_size=100)
         eval_env = ss.frame_stack_v1(eval_env, args.frame_stack)
 
     utils.make_dir(args.work_dir)
