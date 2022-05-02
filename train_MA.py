a6bb3e49def5da10d7591c7e2214ef706c915eba
wwongkamjan <w.wongkamjan@gmail.com>
2022-05-01 23:46:33 -0700

    framestack fix

--- train_MA.py
+++ train_MA.py
@@ -302,7 +302,7 @@ def main():
 
     episode, episode_reward, done = 0, 0, False
     start_time = time.time()
-    env.reset()
+    obs = env.reset()
     step=0
     for agent_iter in env.agent_iter(max_iter=2*args.num_train_steps):
         if step>args.num_train_steps:
